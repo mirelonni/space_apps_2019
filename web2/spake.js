3129bf7089992542608a05d79aabb720015bb1cb
Mircea Cojocaru <mirelonni@gmail.com>
2019-10-19 23:25:53 +0300

    overlapped stars

--- web2/spake.js
+++ web2/spake.js
@@ -1,6 +1,6 @@
 var config = {
     type: Phaser.AUTO,
-    parent: 'phaser-example',
+    // parent: 'phaser-example',
     width: 800,
     height: 600,
     physics: {
@@ -30,43 +30,54 @@ var accelerate = 1;
 var game = new Phaser.Game(config);
 
 function preload() {
+
+    // this.load.setBaseURL('http://127.0.0.1:8080/')
+
     // this.load.image('bullet', 'assets/spake.png');
-    this.load.image('ship', 'assets/spake.png');
+    this.load.image('ship', 'assets/Spake_Satelit-01.png');
 
     //preloading debris intro the image cache
-    // this.load.image('antenna', 'assets/Spake_Antenna-01.png');
-    // this.load.image('dish', 'assets/Spake_Dish-01.png');
-    // this.load.image('solar1', 'assets/Spake_PanouSolar1-01.png');
-    // this.load.image('solar2', 'assets/Spake_PanouSolar2-01.png');
-    // this.load.image('pipe', 'assets/Spake_Pipe-01.png');
-    // this.load.image('nut', 'assets/Spake_Piulita-01.png');
-    // this.load.image('screw', 'assets/Spake_Surub-01.png');
+    this.load.image('antenna', 'assets/Spake_Antenna-01.png');
+    this.load.image('dish', 'assets/Spake_Dish-01.png');
+    this.load.image('solar1', 'assets/Spake_PanouSolar1-01.png');
+    this.load.image('solar2', 'assets/Spake_PanouSolar2-01.png');
+    this.load.image('pipe', 'assets/Spake_Pipe-01.png');
+    this.load.image('nut', 'assets/Spake_Piulita-01.png');
+    this.load.image('screw', 'assets/Spake_Surub-01.png');
+    // this.load.image('deb', 'assets/Spake_Surub-01.png');
 }
 
 function create() {
 
-    this.add.image(400, 300, 'spake');
+    // this.add.image(400, 300, 'spake');
 
     //create the ship sprite from image.
     ship = this.physics.add.image(400, 300, 'ship');
     ship.setDamping(false);
-    ship.setDrag(0.9);
+    // ship.setDrag(0.9);
     ship.setScale(0.25);
     ship.setMaxVelocity(50);
 
     //create the debris group
 
-    // debris = this.add.group();
-    // debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'antenna').setScale(0.2);
-    // debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'dish').setScale(0.2);
-    // debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'solar1').setScale(0.2);
-    // debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'solar2').setScale(0.2);
-    // debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'pipe').setScale(0.2);
-    // debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'nut').setScale(0.2);
-    // debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'screw').setScale(0.2);
+    // debris = this.physics.add.group({
+    //     key: 'deb',
+    //     setXY: { x: 100, y: 100 }
+    // });
+
+    debris = this.physics.add.group();
+    debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'antenna').setScale(0.2);
+    debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'dish').setScale(0.2);
+    debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'solar1').setScale(0.2);
+    debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'solar2').setScale(0.2);
+    debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'pipe').setScale(0.2);
+    debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'nut').setScale(0.2);
+    debris.create(360 + Math.random() * 200, 120 + Math.random() * 200, 'screw').setScale(0.2);
 
 
     cursor = this.input.keyboard.createCursorKeys();
+
+    this.physics.add.overlap(ship, debris, collectGarbage, null, this);
 
     // text = this.add.text(10, 10, '', { font: '16px Courier', fill: '#00ff00' });
 }
@@ -95,6 +106,14 @@ function update() {
         // logo.setVelocity(0, 0);
     }
 
+
+
+    // for (deb in debris) {
+    //     if (this.intersects(ship, deb)) {
+    //         console.log(deb)
+    //     }
+    // }
+
     // text.setText('FUCK JAVASCRIPT!: ' + ship.body.speed);
 
     this.physics.world.wrap(ship, 0);
@@ -102,5 +121,13 @@ function update() {
 
     //giving the debris a angularvelocity
 
+    // console.log("1")
 
+
+}
+
+
+function collectGarbage(ship, deb) {
+    deb.disableBody(true, true);
+    console.log("1")
 }
