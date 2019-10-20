c3e75f33b5c40dddbdba59331b3be9ce90943532
Mircea Cojocaru <mirelonni@gmail.com>
2019-10-20 11:52:03 +0300

    Cleaned repo

--- web_app/globe.js
+++
@@ -1,9 +0,0 @@
-var wwd = new WorldWind.WorldWindow('canvasOne');
-
-wwd.addLayer(new WorldWind.BMNGLayer());
-// wwd.addLayer(new WorldWind.BMNGLandsatLayer());
-wwd.addLayer(new WorldWind.AtmosphereLayer());
-
-// wwd.addLayer(new WorldWind.CompassLayer());
-// wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
-// wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
